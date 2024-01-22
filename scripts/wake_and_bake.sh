a8f142ac85521a456a7f3c5ec2ccdd7ef4f8ffab
subashshakya <subash.shakya@bajratechnologies.com>
2024-01-22 22:00:53 +0545

    feat: implement chaincode to report hub channel

--- scripts/wake_and_bake.sh
+++ scripts/wake_and_bake.sh
@@ -14,14 +14,13 @@ rm -rf java/wallet/*
 rm -rf typescript/wallet/*
 echo COMPLETE
 
-pushd ../fabric-samples/test-network
+pushd ../fabric-samples/test-network/
 
 ./network.sh down
-./network.sh up
+./network.sh up createChannel -c report-hub-channel
 
 # optional for invoking a peer to invoke a chaincode function
 export PATH=${PWD}/../bin:$PATH
-export FABRIC_CFG_PATH=$PWD/../config/
 
 echo "<<============ PACKAGING CHAINCODE =============>>"
 peer lifecycle chaincode package reports.tar.gz --path ${CHAINCODE_PATH} --lang golang --label report-hub_1.0
@@ -56,18 +55,45 @@ echo $PACKAGE_ID
 
 # setting env variables for approving chaincode in org1 peer
 export CC_PACKAGE_ID=$PACKAGE_ID
+echo "<============================= CORE PEER MSP CONFIG PATH ========================================>"
+
+export FABRIC_CFG_PATH=$PWD/configtx/
+echo $FABRIC_CFG_PATH
+
+echo "<==================== CONFIGTX PATH =======================>"
+
+echo ${PWD}/configtx/
+configtxgen -configPath ${PWD}/configtx/
+
+echo "<================================ CREATING GENESIS BLOCK ================================>"
+configtxgen -outputBlock report-hub-channel.block -profile ChannelUsingRaft -channelID report-hub-channel
+configtxgen -profile ChannelUsingRaft -channelID report-hub-channel -outputCreateChannelTx report-hub-channel.tx
+
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
+export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
+
+osnadmin channel join --channelID report-hub-channel --config-block ./report-hub-channel.block -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+
+osnadmin channel list -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+# setting anchor peer
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+echo $CORE_PEER_MSPCONFIGPATH
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_ADDRESS=localhost:7051
 
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --name ${CHAINCODE_NAME} --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+echo ${PWD}/../config/orderer.yaml
 
-peer lifecycle chaincode checkcommitreadiness --name ${CHAINCODE_NAME} --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
+./network.sh deployCC -c report-hub-channel -ccn ${CHAINCODE_NAME} -ccp ${CHAINCODE_PATH} -ccl ${CHAINCODE_LANGUAGE}
 
-peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --name ${CHAINCODE_NAME} --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt"
-
-# for deploying a channel with specified parameters
-./network.sh deployCC -ccn ${CHAINCODE_NAME} -ccp ${CHAINCODE_PATH} -ccl ${CHAINCODE_LANGUAGE}
+popd
 
 ./get_user_msp.sh
